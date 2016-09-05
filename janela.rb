fc0a9c5d0755fc0f38183f2f582f78ec979dfe67
Ionar <irm@ionar.com.br>
2016-09-05 16:50:55 -0300

    botao importar e exportar

--- janela.rb
+++ janela.rb
@@ -4,44 +4,65 @@ require 'tk'
 # Cria janela
 root = TkRoot.new { 
 	title "Imobal"
-	minsize(500,500)
-	geometry '500x500'
+	minsize(400,50)
+	maxsize(400,50)
+	geometry '400x50+300+40'
+	resizable = false, false
 }
+
+# Criando os botoes
+btn_salvar = TkButton.new(root) {
+	state 'active'
+	text "Pasta destino..."
+	pack("side" => "left",  "padx"=> "5", "pady"=> "5")
+}
+
+btn_abrir = TkButton.new(root){
+	state 'disabled'
+	text 'Arquivos a processar...'
+	pack("side" => "left",  "padx"=> "5", "pady"=> "5")
+}
+
+# Botao escolher diretorio - comando
+def chooseDirectory
+	return Tk::chooseDirectory
+end
+selected_directory = TkVariable.new()
+btn_salvar.command(
+	Proc.new{
+		selected_directory = chooseDirectory
+		puts selected_directory
+		if selected_directory != ''
+			btn_abrir.state = 'normal'
+		end
+	}
+)
 
 # Botao abrir arquivos
 def openFiles
 	return Tk.getOpenFile(  'title' => 'Selecione os arquivos',
                         'multiple' => true)
 end
-
 arquivos = TkVariable.new()
-arquivo_csv = TkVariable.new()
-
-btn_abrir = TkButton.new(root){
-	text 'Importar arquivos'
-	command (proc {
+btn_abrir.command(
+	proc{
 		arquivos.value = openFiles;
 		arquivos.to_a.each do |filename|
-			puts filename + " ionar"
+			#puts filename
 			File.open(filename) do |file|
+				nome = File.basename(filename)
+				arquivo_csv = File.open(selected_directory + '/' + nome + '.BIESSE.csv', 'w')
 				conteudo = file.read
-				puts conteudo
 				arquivo_csv.print conteudo
+				arquivo_csv.close
+
+				puts 'NOME: ' + filename
+				puts conteudo
 			end
 		end
-		})
-	pack("side" => "left",  "padx"=> "5", "pady"=> "5")
-}
-
-
-
-btn_salvar = TkButton.new(root) {
-	text "Selecionar diretorio"
-	command Proc.new {
-		Tk::chooseDirectory
+		Tk::messageBox :message => "#{arquivos.to_a.length} arquivos processados"
 	}
-	pack("side" => "left",  "padx"=> "5", "pady"=> "5")
-}
+)
 
 # Executa o loop
 Tk.mainloop
